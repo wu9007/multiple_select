deeb0a8785a1468ece868c45c4087bcc4b4ca7a4
leyan95 <18231926271@163.com>
2019-11-07 10:26:05 +0800

    🤔search bar.

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatefulWidget {
+  final String placeholder;
+
+  SearchPage({this.placeholder});
+
   @override
   State<StatefulWidget> createState() => SearchPageState();
 }
@@ -10,22 +14,31 @@ class SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.search),
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black54),
+        backgroundColor: Colors.white,
+        title: ConstrainedBox(
+          constraints: BoxConstraints(maxHeight: 30, maxWidth: 250),
+          child: TextField(
+            style: TextStyle(fontSize: 13),
+            cursorColor: Colors.grey,
+            cursorWidth: 1.5,
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.only(top: 0, bottom: 0, left: 10),
+                fillColor: Colors.black12,
+                filled: true,
+                suffixIcon: Icon(
+                  Icons.search,
+                  size: 20,
+                  color: Colors.grey,
                 ),
-              ),
-            ),
-            MaterialButton(
-              child: Text('搜索', style: TextStyle(color: Colors.white, fontSize: 16)),
-              onPressed: () => print('hello'),
-            )
-          ],
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(25.0), borderSide: BorderSide.none),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: this.widget.placeholder ?? 'search……'),
+          ),
         ),
       ),
       body: null,
